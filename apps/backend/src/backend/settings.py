85f5b13db7b12219927168b8fc2e95bf0e080202
Siddharth Rao <siddrao.926@gmail.com>
2024-12-26 22:07:21 -0800

    Changed Historic Years To A List Of Ints

--- apps/backend/src/backend/settings.py
+++ apps/backend/src/backend/settings.py
@@ -2,15 +2,15 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    HISTORIC_YEARS: list[str] = [
-        "2016",
-        "2017",
-        "2018",
-        "2019",
-        "2020",
-        "2022",
-        "2023",
-        "2024",
+    HISTORIC_YEARS: list[int] = [
+        2016,
+        2017,
+        2018,
+        2019,
+        2020,
+        2022,
+        2023,
+        2024,
     ]
 
 
