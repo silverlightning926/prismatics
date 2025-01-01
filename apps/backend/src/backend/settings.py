a578517fe65e8a912cbe682ce6adbb15b96ab950
Siddharth Rao <siddrao.926@gmail.com>
2024-12-31 19:59:54 -0800

    Added More Historic Years

--- apps/backend/src/backend/settings.py
+++ apps/backend/src/backend/settings.py
@@ -3,6 +3,20 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     HISTORIC_YEARS: list[int] = [
+        2002,
+        2003,
+        2004,
+        2005,
+        2006,
+        2007,
+        2008,
+        2009,
+        2010,
+        2011,
+        2012,
+        2013,
+        2014,
+        2015,
         2016,
         2017,
         2018,
